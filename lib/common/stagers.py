c22646f28c5d9d893392cdc98a69eea097258df2
VTCAKAVSMoACE <vtcakavsmoace@gmail.com>
2017-05-25 21:31:15 +0100

    Fix small typo preventing launcher generation

--- lib/common/stagers.py
+++ lib/common/stagers.py
@@ -238,7 +238,7 @@ class Stagers:
         if "https" in stage0uri:
             # monkey patch ssl woohooo
             launcherBase += "import ssl;\nif hasattr(ssl, '_create_unverified_context'):ssl._create_default_https_context = ssl._create_unverified_context;\n"
-            stage0url = stage0url.replace("https:", "http:");
+            stage0uri = stage0uri.replace("https:", "http:");
 
         launcherBase += "import sys, urllib2;"
         try:
